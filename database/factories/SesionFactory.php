c3ba42290bf18abb97eb5c2c7f05eb25065637fc
Pablo Escalante <pabloescalante200027@gmail.com>
2026-06-19 09:24:28 -0500

    stand by

--- database/factories/SesionFactory.php
+++ database/factories/SesionFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Sesion;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -18,7 +19,8 @@ class SesionFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'terapeuta_id'    => User::factory(),
+            'paciente_id'    => User::factory(),
         ];
     }
 }
